088f01657c208b409471879c58e2a59646cf048b
Mariia Chudakov <masterova260989@gmail.com>
2024-11-03 03:22:18 +0200

    2 iteration

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
+const { castError, documentNotFoundError } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
+router.use((req, res) => {
+  res
+    .status(documentNotFoundError)
+    .send({ message: "Requested resource not found" });
+});
 
 module.exports = router;
