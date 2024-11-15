2b62ebbec77ff42e0bd18728b91e960691f7d0ef
Mariia Chudakov <masterova260989@gmail.com>
2024-11-15 17:52:19 +0200

    fix eslint 2

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -9,7 +9,7 @@ const {
   dislikeItem,
 } = require("../controllers/clothingItems");
 
-router.get("/", auth, getItems);
+router.get("/", getItems);
 router.post("/", auth, createItem);
 router.delete("/:itemId", auth, deleteItem);
 
